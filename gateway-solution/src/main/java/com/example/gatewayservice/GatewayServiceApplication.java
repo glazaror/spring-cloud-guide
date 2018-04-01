130918f27ef10cf03f28f2c9a971ba1d4563fd9f
Gerardo Lazaro <glazaror@gmail.com>
2018-04-01 18:17:40 -0500

    Mas ejemplos Gateway

--- gateway-solution/src/main/java/com/example/gatewayservice/GatewayServiceApplication.java
+++ gateway-solution/src/main/java/com/example/gatewayservice/GatewayServiceApplication.java
@@ -4,7 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.gateway.discovery.DiscoveryClientRouteDefinitionLocator;
+import org.springframework.cloud.gateway.filter.factory.PrefixPathGatewayFilterFactory;
 import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
@@ -13,6 +15,16 @@ public class GatewayServiceApplication {
 	@Bean
 	DiscoveryClientRouteDefinitionLocator discoveryRoutes(DiscoveryClient dc) {
 		return new DiscoveryClientRouteDefinitionLocator(dc);
+	}
+
+	@Bean
+	public RouteLocator myRoutes(RouteLocatorBuilder builder) {
+		return builder.routes()
+				.route("", r -> r.path("/api/")
+						//.uri("http://httpbin.org:80"))
+						//.uri("lb://eureka-client-solution-sentence/sentence"))
+						.uri("http://localhost:8081/eureka-client-solution-sentence/sentence"))
+				.build();
 	}
 
 	public static void main(String[] args) {
